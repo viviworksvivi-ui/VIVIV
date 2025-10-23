6b9e5f9fe864ff1829f009d257c3374848de6257
habibbellara <votre-email@example.com>
2025-10-23 13:30:24 +0100

    Update Google Analytics tracking ID to G-DJFMTH69DS

--- app/layout.tsx
+++ app/layout.tsx
@@ -47,14 +47,14 @@ export default function RootLayout({
         <meta name="msapplication-TileImage" content="/images/vivi-logo.png" />
         
         {/* Google tag (gtag.js) */}
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-GDV9ZX9FN3"></script>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-DJFMTH69DS"></script>
         <script
           dangerouslySetInnerHTML={{
             __html: `
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
-              gtag('config', 'G-GDV9ZX9FN3');
+              gtag('config', 'G-DJFMTH69DS');
             `,
           }}
         />
